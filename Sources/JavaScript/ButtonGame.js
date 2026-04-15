5b19bbce1828a1087a6c2b9bec7fd4d830015a6a
Zatharis <darkblaze606@gmail.com>
2026-04-15 07:16:02 -0400

    made a game to hide

--- Sources/JavaScript/ButtonGame.js
+++ Sources/JavaScript/ButtonGame.js
@@ -6,4 +6,19 @@ let count = 0
 buttonClick.addEventListener('click', () => {
     count ++;
     buttonCounter.textContent = `Clicked ${count} times`;
+    console.log(count);
+    if (count == 5) {
+        alert("Woah, found something?");
+        buttonClick.textContent = "Does it do something..?"
+    }
+    else if (count == 25) {
+        alert("Hey, aren't you supposed to be working?")
+    }
+    else if (count == 100) {
+        alert("You really want that clicker game huh..")
+        buttonClick.textContent = "Clicky clicky!"
+    }
+    else if (count == 150){
+        alert("Achievement: clicker pro!")
+    }
 })
