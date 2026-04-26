0c8f68367f36879169d1fcc11049937b717974f8
Dylan Camlis <DC218@myscc.ca>
2026-04-25 20:45:48 -0400

    Revert "test 5"
    This reverts commit 10fe02370969e84a9a4e149da3f20cee0f68b729.

--- assets/js/loadComponents.js
+++ assets/js/loadComponents.js
@@ -1,27 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const navPlaceholder = document.
-    getElementById("nav-placeholder");
+    const navPlaceholder = document.getElementById("nav-placeholder");
 
-    const isInPagesFolder = window.location.pathname.includes("/pages/");
-
-    const navPath = isInPagesFolder
-        ? "../assets/components/nav.html"
-        : "assets/components/nav.html";
-
-    fetch(navPath)
-        .then(response => response.text())
-        .then(data => {
-            navPlaceholder.innerHTML = data;
-        })
-        .catch(error => console.error("Error loading nav:", error));
-
-
-    // if (navPlaceholder) {
-    //     fetch("assets/components/nav.html")
-    //         .then(response => response.text())
-    //         .then(data => {
-    //             navPlaceholder.innerHTML = data;
-    //         })
-    //         .catch(error => console.error("Error loading nav:", error));
-    // }
+    if (navPlaceholder) {
+        fetch("assets/components/nav.html")
+            .then(response => response.text())
+            .then(data => {
+                navPlaceholder.innerHTML = data;
+            })
+            .catch(error => console.error("Error loading nav:", error));
+    }
 });
