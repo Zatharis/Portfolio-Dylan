10fe02370969e84a9a4e149da3f20cee0f68b729
Dylan Camlis <DC218@myscc.ca>
2026-04-25 20:34:23 -0400

    test 5

--- assets/js/loadComponents.js
+++ assets/js/loadComponents.js
@@ -1,12 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const navPlaceholder = document.getElementById("nav-placeholder");
+    const navPlaceholder = document.
+    getElementById("nav-placeholder");
 
-    if (navPlaceholder) {
-        fetch("assets/components/nav.html")
-            .then(response => response.text())
-            .then(data => {
-                navPlaceholder.innerHTML = data;
-            })
-            .catch(error => console.error("Error loading nav:", error));
-    }
+    const isInPagesFolder = window.location.pathname.includes("/pages/");
+
+    const navPath = isInPagesFolder
+        ? "../assets/components/nav.html"
+        : "assets/components/nav.html";
+
+    fetch(navPath)
+        .then(response => response.text())
+        .then(data => {
+            navPlaceholder.innerHTML = data;
+        })
+        .catch(error => console.error("Error loading nav:", error));
+
+
+    // if (navPlaceholder) {
+    //     fetch("assets/components/nav.html")
+    //         .then(response => response.text())
+    //         .then(data => {
+    //             navPlaceholder.innerHTML = data;
+    //         })
+    //         .catch(error => console.error("Error loading nav:", error));
+    // }
 });
